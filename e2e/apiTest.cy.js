83c706d10a56571eda78b187315f2e39ff2bbea6
Siew, Sean <sean.siew@rakuten.com>
2022-06-17 22:48:59 +0900

    tweaked api test

--- e2e/apiTest.cy.js
+++ e2e/apiTest.cy.js
@@ -5,7 +5,6 @@ describe('API Test', () => {
     cy.request('POST', 'https://jsonplaceholder.typicode.com/posts', { userId: '50',id:101,title:"Testing123",body:"test" })
     .then((response) => {
     expect(response.body).to.have.property('userId', '50')
-    expect(response.body).to.have.property('id', 101)
     expect(response.body).to.have.property('title', 'Testing123')
     expect(response.body).to.have.property('body', 'test')
   })
