b80f158430fa0d5d47c430a01c6c78f0b7c70395
Siew, Sean <sean.siew@rakuten.com>
2022-06-17 22:28:08 +0900

    api test added

---
+++ e2e/apiTest.cy.js
@@ -0,0 +1,15 @@
+describe('API Test', () => {
+  it('passes', () => {
+    cy.request('https://catfact.ninja/fact').as('cat')
+    cy.get('@cat').its('status').should('equal',200)
+    cy.request('POST', 'https://jsonplaceholder.typicode.com/posts', { userId: '50',id:101,title:"Testing123",body:"test" })
+    .then((response) => {
+    expect(response.body).to.have.property('userId', '50')
+    expect(response.body).to.have.property('id', 101)
+    expect(response.body).to.have.property('title', 'Testing123')
+    expect(response.body).to.have.property('body', 'test')
+  })
+
+
+  })
+})
